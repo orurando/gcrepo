a87fcecb4355e6d6da9fe757d0a7802ea08b8e87
orurando <dimax6@gmail.com>
2015-09-02 03:15:52 -0430

    cambios de ofic mas noche

--- mail-contact-full.php
+++ mail-contact-full.php
@@ -1,22 +1,26 @@
 <?php
 if(isset($_POST['email'])) {
 
-$email_to = "sales@grandcanals.com";
-$email_subject = "Contact from the web";
+$email_to = "dimax6@gmail.com";
+
 
 if(!isset($_POST['full_name']) ||
 !isset($_POST['email']) ||
-!isset($_POST['telephone'])) {
+!isset($_POST['telephone']) ||
+!isset($_POST['company']) ||
+!isset($_POST['subject']) ||
+!isset($_POST['comment'])) {
 
 
-die();
+var_dump($_POST);die();
 }
-
+$email_subject = $_POST['subject'];
 $email_message = "Contact Details:\n\n";
 $email_message .= "Full Name: " . $_POST['full_name'] . "\n";
 $email_message .= "E-mail: " . $_POST['email'] . "\n";
 $email_message .= "Phone Number: " . $_POST['telephone'] . "\n";
 $email_message .= "Company: " . $_POST['company'] . "\n\n";
+$email_message .= "Comment: " . $_POST['comment'] . "\n\n";
 
 
 $headers = 'From: '.$email_from."\r\n".
